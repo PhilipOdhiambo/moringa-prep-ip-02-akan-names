552e7c28a00fcb4e88953ee70dac10acb4c3fd34
Philip Odhiambo Owino <jofonowino@gmail.com>
2021-06-10 17:55:51 +0300

    add function to validate input

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-var year = document.getElementById('year');
-var month = document.getElementById('month');
-var day = document.getElementById('month');
-var gender  = document.getElementById('gender');
+var year = document.getElementById('year').value;
+var month = document.getElementById('month').value;
+var day = document.getElementById('month').value;
+var gender  = document.getElementById('gender').value;
+var date = 50;
+
+// Function to validate date and year
+function validateInput(dateInput,yearInput) {
+    var date = parseInt(dateInput);
+    var year = parseInt(yearInput);
+    // Check if year is four digits
+    if (yearInput.length !== 4 || isNaN(year)) {
+        console.log("invalid Year");
+    } else if (date < 1 || date > 31) {
+        console.log("invalid date")
+    } else {
+        calculateDay(date, month, year)
+    }
+}
