7b6fca41e55e55b586a804ecdfebe655d78ee1b7
Philip Odhiambo Owino <jofonowino@gmail.com>
2021-06-10 15:29:34 +0300

    add gender select option to script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 var year = document.getElementById('year');
 var month = document.getElementById('month');
 var day = document.getElementById('month');
+var gender  = document.getElementById('gender');
