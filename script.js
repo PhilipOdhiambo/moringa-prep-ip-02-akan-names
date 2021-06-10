c46d10457729dadeed60992678b48f25e32e276f
Philip Odhiambo Owino <jofonowino@gmail.com>
2021-06-10 18:18:24 +0300

    add function to calculate day

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ function validateInput(dateInput,yearInput) {
         calculateDay(date, month, year)
     }
 }
+
+// Function to caluculate Day
+function calculateDay(date,month,year) {
+  /* 
+    Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7;
+    where;
+    CC - is the century digits. For example 1989 has CC = 19
+    YY - is the Year digits (1989 has YY = 89)
+    MM -  is the Month
+    DD - is the Day of the month
+    mod - is the modulus function ( % )
+ */ 
+    var cc = parseInt(year.substr(0,2));
+    var yy = parseInt(year.substr(2));
+
+    
+    var day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+}
+
+validateInput(3,"1234");
